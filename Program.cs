afacc4566df358b482bd6d139750352d6e63debc
mysteryboy73 <mysteryboy7300@gmail.com>
2017-05-19 06:19:40 -0400

    Trying to write contents of response to file but getting a time out.

--- Program.cs
+++ Program.cs
@@ -7,20 +7,25 @@ namespace ClashOfClans
     {
         static void Main(string[] args)
         {
-            var client = new RestClient("http://api.clashofclans.com/v1/");
+            var client = new RestClient("http://api.clashofclans.com/v1");
             
             var request = new RestRequest("clans", Method.GET);
             request.AddParameter("name", "in yo face");
 
             request.AddHeader("Accept", "application/json");
-            request.AddHeader("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjA3NmViYjE4LWI1N2EtNGJjYS04Yjk0LWNmYWNiNWYwMzk5NyIsImlhdCI6MTQ5NTE0NTg1Nywic3ViIjoiZGV2ZWxvcGVyLzEzNmQ0NzI1LTYyMWItMmY1OC1iNTcwLWM1ZGY2ZmVmN2U2MyIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjE0OC4xNDEuMTUuOTciXSwidHlwZSI6ImNsaWVudCJ9XX0.SCN9usmlxkMMj2s_yt647q9NtIN-YcMtpiv1gxIFqj3y5Vb_Mf8aDsXxkXtqAMLh-nzeQWFRFN_5YdckkyM0Ng");
+            request.AddHeader("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjNkMDkzZThhLWMxNTAtNDI1NS04NTA4LWMzYTBlMTU1MTMyNiIsImlhdCI6MTQ5NTE1MzMyNSwic3ViIjoiZGV2ZWxvcGVyLzEzNmQ0NzI1LTYyMWItMmY1OC1iNTcwLWM1ZGY2ZmVmN2U2MyIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjEwOC44NC4xOTguMTQiXSwidHlwZSI6ImNsaWVudCJ9XX0.hIjNh83gOEbkbLdaQn-9u8gkSxFuyOAoDaq1tarxlvpKolZh5kKQYP9MCGhitYSaeJNUuJwIm7kUTXeHXZPYug");
 
+            Console.WriteLine("Sending request....");
+            var response = client.Execute(request);
+            Console.WriteLine("Request Completed....");
 
-            IRestResponse response = client.Execute(request);
 
             var responseContent = response.Content;
+            Console.WriteLine("Sending request content to file...");
+            var file = new System.IO.StreamWriter("..\\testApiCall.txt");
+            file.WriteLine("This is the response: " + responseContent);
+            file.Close();
 
-            Console.Write(responseContent);
             Console.ReadLine();
         }
     }
