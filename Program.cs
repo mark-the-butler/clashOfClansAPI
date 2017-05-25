79b5f2027bcd62f3137a348900f87e0218032008
mysteryboy73 <mysteryboy7300@gmail.com>
2017-05-24 21:43:57 -0400

    Changed url request

--- Program.cs
+++ Program.cs
@@ -9,8 +9,8 @@ namespace ClashOfClans
         {
             var client = new RestClient("https://api.clashofclans.com/v1/");
 
-            var request = new RestRequest("clans", Method.GET);
-            request.AddParameter("name", "in yo face");
+            var request = new RestRequest("clans/%238ujgproj", Method.GET);
+            request.AddParameter("clanTag", "%238ujgproj", ParameterType.UrlSegment);
 
             request.AddHeader("Accept", "application/json");
             request.AddHeader("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjNkMDkzZThhLWMxNTAtNDI1NS04NTA4LWMzYTBlMTU1MTMyNiIsImlhdCI6MTQ5NTE1MzMyNSwic3ViIjoiZGV2ZWxvcGVyLzEzNmQ0NzI1LTYyMWItMmY1OC1iNTcwLWM1ZGY2ZmVmN2U2MyIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjEwOC44NC4xOTguMTQiXSwidHlwZSI6ImNsaWVudCJ9XX0.hIjNh83gOEbkbLdaQn-9u8gkSxFuyOAoDaq1tarxlvpKolZh5kKQYP9MCGhitYSaeJNUuJwIm7kUTXeHXZPYug");
